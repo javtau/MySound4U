ada2da153fa8c1d038d571fbec2602d156f12b3c
Fernando <fernando.barroso@estudiante.uam.es>
2019-03-27 15:06:14 +0100

    Demostrador

--- MySound4U/src/principal/Demostrador2.java
+++ MySound4U/src/principal/Demostrador2.java
@@ -54,13 +54,12 @@ public class Demostrador2 {
 
 		// Parar una cancion
 
-		System.out.println("Se va a parar la cancion en 10 segundos");
+		System.out.println("Se va a parar la cancion en 10 segundos\n");
 		TimeUnit.SECONDS.sleep(2);
 		anonima.stop();
 
 		// Metodo buscar
 
-		System.out.println("");
 		System.out.println("************");
 		System.out.println("* Busqueda *");
 		System.out.println("************");
@@ -70,10 +69,10 @@ public class Demostrador2 {
 		api.buscar("levels", TIPO_BUSQUEDA.TODO);
 		TimeUnit.SECONDS.sleep(2);
 
-		System.out.println("\nVamos a buscar la cancion \"RookiezisPunk\" aplicando el filtro \"todo\": ");
+		System.out.println("Vamos a buscar la cancion \"RookiezisPunk\" aplicando el filtro \"todo\": ");
 		TimeUnit.SECONDS.sleep(2);
 		api.buscar("rook", TIPO_BUSQUEDA.TODO);
-		System.out.println("\nLa cancion es explicita y, al ser un usuario anonimo, no esta disponible para el");
+		System.out.println("La cancion es explicita y, al ser un usuario anonimo, no esta disponible para el");
 		TimeUnit.SECONDS.sleep(2);
 
 		// api.buscar("", TIPO_BUSQUEDA.ALBUM);
@@ -83,31 +82,30 @@ public class Demostrador2 {
 		api.buscar("core", TIPO_BUSQUEDA.TITULO);
 		TimeUnit.SECONDS.sleep(2);
 
-		System.out.println("\nVamos a buscar las canciones del autor \"System\" aplicando el filtro \"autor\": ");
+		System.out.println("Vamos a buscar las canciones del autor \"System\" aplicando el filtro \"autor\": ");
 		TimeUnit.SECONDS.sleep(2);
 		api.buscar("system", TIPO_BUSQUEDA.AUTOR);
 		TimeUnit.SECONDS.sleep(2);
 
 		// Metodo registrarse
 
-		System.out.println("");
 		System.out.println("************");
 		System.out.println("* Registro *");
 		System.out.println("************");
 
 		System.out.println(
-				"\nVamos a registrarnos en la aplicacion con nombre de usuario: \"Fernando\", contrasena: \"1234\" y menor de edad");
+				"\nVamos a registrarnos en la aplicacion con nombre de usuario: \"Fernando\", contrasena: \"1234\". El usuario sera menor de edad\n");
 		TimeUnit.SECONDS.sleep(2);
 		api.registrarse("Fernando", "1234", "14/10/2018");
 		TimeUnit.SECONDS.sleep(2);
 
-		System.out.println("\nVamos a registrarnos en la aplicacion con el mismo nombre de usuario");
+		System.out.println("\nVamos a registrarnos en la aplicacion con el mismo nombre de usuario\n");
 		TimeUnit.SECONDS.sleep(2);
 		api.registrarse("Fernando", "1234", "14/10/2018");
 		TimeUnit.SECONDS.sleep(2);
 
 		// Metodo login
-
+		System.out.println("\n");
 		System.out.println("********************");
 		System.out.println("* Inicio de sesion *");
 		System.out.println("********************");
@@ -117,22 +115,23 @@ public class Demostrador2 {
 		TimeUnit.SECONDS.sleep(2);
 
 		// USUARIO REGISTRADO
-		
+
 		// Reproducir una cancion
-		
+
 		canciones = api.getLastSongs();
-		
+
 		System.out.println("****************");
 		System.out.println("* Reproduccion *");
 		System.out.println("****************");
-
+		System.out.println(canciones.get(1));
+		System.out.println("Se va a reproducir la cancion");
 		usuario.reproducir(canciones.get(1));
 		TimeUnit.SECONDS.sleep(2);
-		System.out.println("Se va a parar la cancion");
+		System.out.println("Se va a parar la cancion en 10 segundos");
 		TimeUnit.SECONDS.sleep(2);
 		usuario.stop();
 		TimeUnit.SECONDS.sleep(2);
-		
+
 		// Metodo buscar
 
 		System.out.println("");
@@ -143,32 +142,37 @@ public class Demostrador2 {
 		System.out.println("\nVamos a buscar la cancion \"RookiezisPunk\" aplicando el filtro \"todo\": ");
 		TimeUnit.SECONDS.sleep(2);
 		api.buscar("rook", TIPO_BUSQUEDA.TODO);
-		System.out.println("\nLa cancion es explicita y el usuario es menor de edad, por tanto, no puede encontrarla.");
-		
-		System.out.println("Vamos a hacer logout de este usuario y registrar otro que sea mayor de edad y comprobar que el si que puede encontrarla y, ademas, reproducirla");
-		
+		System.out.println("La cancion es explicita y el usuario es menor de edad, por tanto, no puede encontrarla.");
+
+		System.out.println(
+				"\nVamos a hacer logout de este usuario y registrar otro que sea mayor de edad y comprobar que el si que puede encontrarla y, ademas, reproducirla");
+
 		System.out.println("");
 		System.out.println("************");
 		System.out.println("* Logout *");
 		System.out.println("************");
 		api.desloguearse();
 		anonima = (SesionAnonima) api.getSesion();
-		
+
+		System.out.println("");
 		System.out.println("************");
 		System.out.println("* Registro *");
 		System.out.println("************");
 		
+		System.out.println(
+				"\nVamos a registrarnos en la aplicacion con nombre de usuario: \"Javi\", contrasena: \"1234\". El usuario sera mayor de edad\n");
 		api.registrarse("Javi", "1234", "14/10/1994");
 		
+		System.out.println("");
 		System.out.println("********************");
 		System.out.println("* Inicio de sesion *");
 		System.out.println("********************");
 		api.loguearse("Javi", "1234");
 		usuario = (SesionUsuarios) api.getSesion();
 		canciones = api.getLastSongs();
-		
+
 		System.out.println("La edad de este usuario es: " + usuario.getUsuarioRegistrado().getEdad());
-		
+
 		System.out.println("");
 		System.out.println("************");
 		System.out.println("* Busqueda *");
@@ -177,18 +181,18 @@ public class Demostrador2 {
 		System.out.println("\nVamos a buscar la cancion \"RookiezisPunk\" aplicando el filtro \"todo\": ");
 		TimeUnit.SECONDS.sleep(2);
 		api.buscar("rook", TIPO_BUSQUEDA.TODO);
-		
+
 		System.out.println("****************");
 		System.out.println("* Reproduccion *");
 		System.out.println("****************");
 
-		System.out.println(canciones.get(1));
+		System.out.println("Se va a reproducir la cancion explicita \"RookiezisPunk\"");
 		TimeUnit.SECONDS.sleep(2);
 		usuario.reproducir(canciones.get(1));
+		TimeUnit.SECONDS.sleep(2);
+		System.out.println("Se va a parar la cancion en 10 segundos");
 		TimeUnit.SECONDS.sleep(10);
 		usuario.stop();
-		
-		
 
 		/*
 		 * api.desloguearse(); api.loguearse("admin", "admin"); administrador =
